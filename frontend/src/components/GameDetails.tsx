ae941b229e5a3c9d77d66cab239fdd9299b416db
srshukla321 <srshukla321@gmail.com>
2023-10-01 18:40:34 +0530

    ui fixed in game details

--- frontend/src/components/GameDetails.tsx
+++ frontend/src/components/GameDetails.tsx
@@ -2,79 +2,106 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const GameDetails = () => {
-  const { id } = useParams<{ id: string }>();
-  const [game, setGame] = useState<any | null>(null);
+    const { id } = useParams<{ id: string }>();
+    const [game, setGame] = useState<any | null>(null);
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/api/getGame/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setGame(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching game details:', error);
-      });
-  }, [id]);
+    const handlePlayButtonClick = () => {
+        const publicKey = localStorage.getItem('publicKey');
+        if (publicKey) {
+            // Construct the API URL with game_id and publicKey
+            const apiUrl = `http://localhost:5000/api/playgame/${id}/${publicKey}`;
 
-  if (!game) {
-    return <div>Loading...</div>;
-  }
+            // Make the API request
+            window.open(apiUrl, '_blank');
+        }
+    };
 
-  return (
-    <div className="bg-gray-900 text-white min-h-screen pt-20 relative">
-      <div className="max-w-screen-lg mx-auto p-6">
-        {/* Banner Image */}
-        <div className="w-full h-96 mb-6">
-          <img
-            src={`data:image/png;base64,${game.banner}`}
-            alt={game.title}
-            className="w-full h-full object-cover rounded-lg shadow-lg"
-          />
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/getGame/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setGame(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching game details:', error);
+            });
+    }, [id]);
+
+    if (!game) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="bg-gray-800 text-white min-h-screen relative">
+            <div className="max-w-screen-lg mx-auto p-6">
+                {/* Banner Image */}
+                <div className="relative w-full h-96 mb-6">
+                    {/* Blurred Background */}
+                    <div
+                        className="absolute inset-0 bg-center bg-cover filter blur-lg blur-3xl"
+                        style={{
+                            backgroundImage: `url(data:image/png;base64,${game.banner})`,
+                        }}
+                    ></div>
+                    {/* Uncropped Image */}
+                    <div className="flex">
+                        <img
+                            src={`data:image/png;base64,${game.banner}`}
+                            alt={game.title}
+                            className="top-48  object-cover rounded-2xl  relative inset-0"
+                        />
+                        <div className="w-full mr-24">
+                            <h1 className="font-bold ml-3 text-3xl top-48 object-cover text-gray-200  bg-gray-900 bg-opacity-40 p-3 w-full rounded-lg  relative inset-0">
+                                {game.title.toUpperCase()}
+                            </h1>
+                            <p className="bg-opacity-10 font-bold top-48 ml-3 relative" style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}>
+                                By: {game.developer}
+                            </p>
+
+                            <p className="bg-opacity-10 font-bold top-48 ml-3 relative" style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}>
+                            Published by: {game.publisher}</p>
+                            <p
+                                className={`top-48 p-1 my-1 font-bold ml-3 w-40 relative inset-0 rounded-lg border-gray-500 border-1.5 ${
+                                    game.price === 0 ? 'bg-green-600' : 'bg-black'
+                                } px-2`}
+                            >
+                                {game.price === 0 ? 'Free to Play' : `Price: ${game.price} SOL`}
+                            </p>
+                            
+                        </div>
+                        
+
+                    </div>
+                    <button
+                        className="absolute bottom-0 right-20 w-40 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                        onClick={handlePlayButtonClick} // Attach the click event handler
+                    >
+                        Play
+                    </button>
+                </div>
+
+                {/* Game Details */}
+                <div className="pt-32">
+                   
+
+                    {/* Description Section */}
+                    <div className="mt-4 bg-gray-900 p-4 rounded">
+                        <h2 className="text-xl font-semibold">Description</h2>
+                        <p className="text-gray-300">{game.description}</p>
+                    </div>
+
+
+                    {/* Release Date Section */}
+                    <div className="mt-4 bg-gray-900 p-4 rounded">
+                        <h2 className="text-xl font-semibold">Release Date</h2>
+                        <p className="text-gray-300">{game.releaseDate}</p>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
-
-        {/* Game Details */}
-        <div>
-          <h1 className="text-3xl font-semibold bg-gray-800 p-2 rounded">
-            {game.title}
-          </h1>
-
-          {/* Description Section */}
-          <div className="mt-4 bg-gray-800 p-4 rounded">
-            <h2 className="text-xl font-semibold">Description</h2>
-            <p className="text-gray-300">{game.description}</p>
-          </div>
-
-          {/* Developer Section */}
-          <div className="mt-4 bg-gray-800 p-4 rounded">
-            <h2 className="text-xl font-semibold">Developer</h2>
-            <p className="text-gray-300">{game.developer}</p>
-          </div>
-
-          {/* Publisher Section */}
-          <div className="mt-4 bg-gray-800 p-4 rounded">
-            <h2 className="text-xl font-semibold">Publisher</h2>
-            <p className="text-gray-300">{game.publisher}</p>
-          </div>
-
-          {/* Release Date Section */}
-          <div className="mt-4 bg-gray-800 p-4 rounded">
-            <h2 className="text-xl font-semibold">Release Date</h2>
-            <p className="text-gray-300">{game.releaseDate}</p>
-          </div>
-
-          {/* Price Section */}
-          <div className="mt-4 bg-gray-800 p-4 rounded">
-            <h2 className="text-xl font-semibold">Price</h2>
-            <p className="text-gray-300">${game.price}</p>
-          </div>
-
-          <button className="mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Buy Now
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default GameDetails;
