fc38929a37eedf8511402c4e4881cd1722431876
AnuragDubeyGame <AnuragDubeyGames.com>
2023-10-03 19:56:29 +0530

    Fixed UserAPI - Final

--- backend/userInfo.js
+++ backend/userInfo.js
@@ -2,11 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserInfo = new Schema({
-    username: { type: String, unique: true },
-    publicKey: String,
-    gamesPurchased: [String],
-  },{
-    collection: 'userInfoCollection',
-  });
-  
+  username: { type: String, unique: true },
+  publicKey: String,
+  gamesPurchased: {
+    type: [String],
+    validate: {
+      validator: function (games) {
+        const nonEmptyGames = games.filter(game => game.trim() !== '');
+        this.gamesPurchased = nonEmptyGames;
+        return true; 
+      },
+    },
+  },
+}, {
+  collection: 'userInfoCollection',
+});
+
  module.exports = mongoose.model("User", UserInfo);
