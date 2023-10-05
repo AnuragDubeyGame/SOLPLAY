f16f93a63cc0ac429a131ee7cb4d0cd6151504d0
AnuragDubeyGame <AnuragDubeyGames.com>
2023-10-05 15:05:24 +0530

    Updated API_URL To AWS DOMAIN

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:5000'
 // export const API_URL = 'https://long-pear-dibbler-gear.cyclic.cloud'
-export const API_URL = 'http://52.66.201.57:5000'
+export const API_URL = 'https://52-66-201-57.nip.io:5000'
